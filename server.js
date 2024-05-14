160a45fc05fcdc2a1bf3759ed62dd793009434db
kristyyip <kristyyip51998@gmail.com>
2024-05-14 12:25:09 -0400

    add get route to /notes

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+// import necessary modules
 const express = require("express");
+const {readFile, writeFile} = require("fs").promises;
+const path = require("path");
 
 const app = express();
 
@@ -6,7 +9,13 @@ const PORT = 3001;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => console.log(res));
+app.get("/", (req, res) => 
+    console.log(res)
+);
+
+app.get("/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
