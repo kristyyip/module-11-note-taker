e1e4d668cf345fbcac1fd757472656f9062ca6a7
kristyyip <kristyyip51998@gmail.com>
2024-05-17 14:42:36 -0400

    changed get route to wildcard to direct to index.html

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.static("public"));
 
 app.use("/api", api)
 
-app.get("/", (req, res) => 
-    console.log(res)
-);
-
 app.get("/notes", (req, res) => 
     res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("*", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.listen(PORT, () =>
